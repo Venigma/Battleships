7fe08f78eae536f4aff65d438776796e79def7ac
Venigma <georgerusbowles@gmail.com>
2015-09-05 19:05:01 +0100

    Alpha 1.0.1

--- Main.py
+++ Main.py
@@ -22,7 +22,7 @@ while SideCorrect:
 
     PlayerSide = raw_input("Your Side: ")  # PlayerSide stores Navy or Pirates acoording to input
 
-    if PlayerSide == "navy" or PlayerSide == "Navy":
+    if PlayerSide == "navy" or PlayerSide == "Navy" or PlayerSide == "n":  # remove "n" eventually
         PlayerSide = "Navy"
         SideCorrect = "True"
         break
@@ -107,7 +107,7 @@ else:
 
 print "\nYour Opponent is known as " + CPUSideFinal
 
-Tutorialq = raw_input("\n\nWould you like to take the tutorial? Yes or No? ")
+Tutorialq = raw_input("\n\nWould you like to take the short tutorial? Yes or No? ")
 
 
 def tutorial():
@@ -118,23 +118,26 @@ def tutorial():
     print "This is the grid:"
     print """
               0 1 2 3 4
-             0~ ~ ~ ~ ~
-             1~ ~ ~ ~ ~
-             2~ ~ ~ ~ ~
-             3~ ~ ~ ~ ~
-             4~ ~ ~ ~ ~"""
+             0 ~ ~ ~ ~ ~
+             1 ~ ~ ~ ~ ~
+             2 ~ ~ ~ ~ ~
+             3 ~ ~ ~ ~ ~
+             4 ~ ~ ~ ~ ~\n"""
+    print "You will see it before and after every turn,\nIf you see an O that means you tried there before and missed."
+    print "If you see a X that means you tried there before and hit"
     raw_input("\n\nPress the enter key to play\n")
     return None
 
 
-if Tutorialq == "Yes" or "yes" or "y" or "Y":
+if Tutorialq == "Yes" or Tutorialq == "yes" or Tutorialq == "y" or Tutorialq == "Y":
     tutorial()
 else:
     print "Ok, Game is Starting"
 
 
+# //////////////////////////////////////////////Game Setup\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-# /////////////////////////////////////////////Game Setup\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+# CPU Board
 
 board = []  # Empty board list
 
@@ -146,7 +149,61 @@ def print_board(board):
     for row in board:
         print " " .join(row)  # " ".join(row) gets rid of commas in list
 
+# Player Board
+# ADD PLAYER OWN CHOOSE SHIP POSTIONS
+player_board = []  # Empty board list
+
+for n in range(0, 5):
+    player_board.append(["~"] * 5)  # Creates a 5x5 grid of ~'s
+
+
+def print_player_board(board):
+    for row in board:
+        print " " .join(row)  # " ".join(row) gets rid of commas in list
 # ////////////////////////////////////////Game Start\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+print "Here is a guide of the grid:"
+print """
+               0 1 2 3 4
+             0 ~ ~ ~ ~ ~
+             1 ~ ~ ~ ~ ~
+             2 ~ ~ ~ ~ ~
+             3 ~ ~ ~ ~ ~
+             4 ~ ~ ~ ~ ~\n"""
+
+print_player_board(player_board)
+
+
+print "You have 4 Ships, choose where to put them"
+
+# Ship 1
+chose1right = "False"
+while chose1right:
+    playership1row = int(raw_input("Ship 1 row: "))
+    playership1col = int(raw_input("Ship 1 Column: "))
+    player_board[playership1row][playership1col] = "V"
+    if playership1row not in range(5) or playership1col not in range(5):
+        chose1right = "False"
+        print "Hmm, they dont seem to be correct coordinates. Try that ship again"
+    else:
+        chose1right = "True"
+        break
+# Ship 2
+playership2row = int(raw_input("Ship 2 row: "))
+playership2col = int(raw_input("Ship 2 Column: "))
+player_board[playership2row][playership2col] = "V"
+# Ship 3
+playership3row = int(raw_input("Ship 3 row: "))
+playership3col = int(raw_input("Ship 3 Column: "))
+player_board[playership3row][playership3col] = "V"
+# Ship 4
+playership4row = int(raw_input("Ship 4 row: "))
+playership4col = int(raw_input("Ship 4 Column: "))
+player_board[playership4row][playership4col] = "V"
+
+# if playership1row not in range(5) or playership1col not in range(5) or playership2row not in range(5) or playership2col not in range(5)or or or :
+
+print_player_board(player_board)
+
 
 print "\n\n\nYour Turn\n"
 print_board(board)
@@ -162,22 +219,34 @@ def random_col(board):
 ship_row = random_col(board)
 ship_col = random_row(board)
 
-# print ship_row # DEBUG OFF
-# print ship_col # DEBUG OFF
+print ship_row  # DEBUG OFF
+print ship_col  # DEBUG OFF
 
-guess_row = int(raw_input("Guess Row: "))
-guess_col = int(raw_input("Guess Column: "))
+goworked = "False"
 
+while goworked:
+    guess_row = int(raw_input("Guess Row: "))
+    guess_col = int(raw_input("Guess Column: "))
 
-if guess_row not in range(5) or guess_col not in range(5):
-        print "\nBe careful were you aim that thing, sailor! \nYou missed entirely, remember the coordinates are from \
-        0,0 to 4,4."
+    if guess_row not in range(5) or guess_col not in range(5):
+            print "\nBe careful were you aim that thing, sailor! \nYou missed entirely, remember the coordinates are \
+            from 0,0 to 4,4."
+            goworked = "False"
 
-elif guess_row == ship_row and guess_col == ship_col:
-    print "\nThats a hit, good work, sailor"
-    board[guess_row][guess_col] = "X"
+    elif guess_row == ship_row and guess_col == ship_col:
+        print "\nThats a hit, good work, sailor"
+        board[guess_row][guess_col] = "X"
+        goworked = "True"
+        break
 
-else:
-    print "\nOh dear, you missed, have another go"
-    board[guess_row][guess_col] = "O"
-    print print_board(board)
+    elif board[guess_row][guess_col] == "O" or board[guess_row][guess_col] == "X":
+        print "\nYou have already tried those coordinates.Try again. "
+        print_board(board)
+        goworked = "False"
+
+    else:
+        print "\nOh dear, you missed, have another try in a minute."
+        board[guess_row][guess_col] = "O"
+        print print_board(board)
+        goworked = "True"
+        break
